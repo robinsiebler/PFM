c554f0290d86de7f92458abf9abe8172214eb6c3
robinsiebler <robinsiebler@dslextreme.com>
2016-04-29 04:31:55 -0700

    Added install requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name='PyFileManager',
+	version='0.0001',
+	packages=[''],
+	url='https://github.com/robinsiebler/PFM',
+	license='',
+	author='Robin Siebler',
+	author_email='robinsiebler@dslextreme.com',
+	description='This is my 1st attempt at creating a file manager using Python and Gtk+'
+)
