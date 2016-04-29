52ce704978a16c7d1a45bb41d741b5ac41af1593
robinsiebler <robinsiebler@dslextreme.com>
2016-04-29 04:33:40 -0700

    Forgot to commit to master

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
 	license='',
 	author='Robin Siebler',
 	author_email='robinsiebler@dslextreme.com',
-	description='This is my 1st attempt at creating a file manager using Python and Gtk+'
+	description='This is my 1st attempt at creating a file manager using Python and Gtk+',
+	install_requires=['gi', 'humanfriendly', 'psutil']
 )
